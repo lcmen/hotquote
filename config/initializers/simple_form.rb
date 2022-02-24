dc25bef8be121f0b2e509afc72feb8bf6b7865d6
Lucas Mendelowski <lm@noreply.codeberg.org>
2022-02-24 21:59:19 +0100

    Implement Quotes CRUD (#1)
    Co-authored-by: Lucas Mendelowski <lucas@mendelowski.com>
    Reviewed-on: https://codeberg.org/lm/hotquote/pulls/1
    Co-authored-by: Lucas Mendelowski <lm@noreply.codeberg.org>
    Co-committed-by: Lucas Mendelowski <lm@noreply.codeberg.org>

---
+++ config/initializers/simple_form.rb
@@ -0,0 +1,23 @@
+SimpleForm.setup do |config|
+  config.wrappers :default, class: "form__group" do |b|
+    b.use :html5
+    b.use :placeholder
+    b.use :label, class: "visually-hidden"
+    b.use :input, class: "form__input", error_class: "form__input--invalid"
+  end
+
+  config.generate_additional_classes_for = []
+  config.default_wrapper = :default
+
+  config.boolean_label_class = 'form__checkbox-label'
+  config.boolean_style = :nested
+
+  config.browser_validations = false
+
+  config.button_class = 'btn'
+
+  config.error_notification_tag = :div
+  config.error_notification_class = 'error_notification'
+
+  config.label_text = lambda { |label, _, _| label }
+end
