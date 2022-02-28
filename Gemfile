88f488d5e4d2d03b0eed079446c31a42cd108413
Lucas Mendelowski <lucas@mendelowski.com>
2022-02-28 21:10:13 +0100

    Introduce basic multitenancy and users

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby "3.0.2"
 
 gem "rails", "~> 7.0.2", ">= 7.0.2.2"
+gem "devise"
 gem "sqlite3", "~> 1.4"
 gem "puma", "~> 5.0"
 gem "simple_form"
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "pages#home"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
   resources :quotes
 end
